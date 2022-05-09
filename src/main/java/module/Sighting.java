a6791fcd62cac234d8a07e265efdea8c5d758244
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-09 18:48:19 +0300

    Add class Sighting with properties

--- src/main/java/module/Sighting.java
+++ src/main/java/module/Sighting.java
@@ -1,4 +1,22 @@
 package module;
 
-public class Sightings {
+
+import java.sql.Timestamp;
+
+public class Sighting {
+    private int animalId;
+    private int id;
+    private String location;
+    private String rangerName;
+    private Timestamp lastSeen;
+
+    public Sightings (int animalId, String location, String rangerName){
+        this.animalId = animalId;
+        this.location = location;
+        this.rangerName = rangerName;
+
+        if (location.isEmpty() || rangerName.isEmpty()){
+            throw new IllegalArgumentException("Please enter all input fields.");
+        }
+    }
 }
