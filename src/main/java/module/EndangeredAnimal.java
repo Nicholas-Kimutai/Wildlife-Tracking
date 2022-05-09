51717a485e22a0f366d0624a02d28f35b50e1a86
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-09 17:58:38 +0300

    Add class EndangeredAnimal with properties

--- src/main/java/module/EndangeredAnimal.java
+++ src/main/java/module/EndangeredAnimal.java
@@ -1,4 +1,15 @@
 package module;
 
-public class EndangeredAnimal {
+public class EndangeredAnimal extends Wildlife implements DatabaseManagement{
+    public static final String ANIMAL_TYPE = "endangered-animal";
+    public  EndangeredAnimal(String name, String health, String age){
+        this.name = name;
+        this.health = health;
+        this.age = age;
+        this.type = ANIMAL_TYPE;
+
+        if (name.isEmpty() || health.isEmpty() || age.isEmpty()){
+            throw new IllegalArgumentException("Please enter all input fields.");
+        }
+    }
 }
