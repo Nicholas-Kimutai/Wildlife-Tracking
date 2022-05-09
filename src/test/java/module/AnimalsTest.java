0651db4bbdeffc7f6a400a758777375e69a9b8e8
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-09 08:29:21 +0300

    Add test cases

--- src/test/java/module/AnimalsTest.java
+++ src/test/java/module/AnimalsTest.java
@@ -1,4 +1,21 @@
 package module;
 
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class AnimalsTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    public Animals setUpAnimal(){
+        return new Animals();
+    }
+    @Test
+    public void animal_instantiatesCorrectly_true(){
+        Animals animal = setUpAnimal();
+        assertEquals(true,animal instanceof Animals);
+    }
 }
