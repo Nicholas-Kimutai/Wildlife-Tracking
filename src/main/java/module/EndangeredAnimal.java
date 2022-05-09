90e351c4c141aa4493cf66426ddfd63d21927899
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-09 18:11:27 +0300

    Save endageredAnimal to database

--- src/main/java/module/EndangeredAnimal.java
+++ src/main/java/module/EndangeredAnimal.java
@@ -1,5 +1,9 @@
 package module;
 
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class EndangeredAnimal extends Wildlife implements DatabaseManagement{
     public static final String ANIMAL_TYPE = "endangered-animal";
     public  EndangeredAnimal(String name, String health, String age){
@@ -22,6 +26,15 @@ public class EndangeredAnimal extends Wildlife implements DatabaseManagement{
             return this.getName().equals(newAnimal.getName()) &&
                     this.getHealth().equals(newAnimal.getHealth()) &&
                     this.getAge().equals(newAnimal.getAge());
+        }
+    }
+
+    public static List<EndangeredAnimal> all() {
+        String sql = "SELECT * FROM animals WHERE type='endangered-animal';";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(EndangeredAnimal.class);
         }
     }
 }
