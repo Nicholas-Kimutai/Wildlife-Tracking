629ff1170b1727488179da9a522a8322f3627ce6
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-10 16:47:39 +0300

    Deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,9 +10,18 @@ import java.util.Map;
 
 import static spark.Spark.*;
 public class App {
-    public static void main(String[] args) {
-        staticFileLocation("/public");
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
         get("/",(request, response) -> {
             Map<String,Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
